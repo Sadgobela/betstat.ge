02ff0640c93fffe58414cabfb13dcc504ef1cb8c
i.sadgobelashvili <sadgobela@gmail.com>
2022-03-18 01:20:02 +0400

    front edits

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
         //
     }
 }
